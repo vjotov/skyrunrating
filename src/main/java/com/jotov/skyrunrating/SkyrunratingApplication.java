9b19dd9881f0d40776255fbd5d2494c02e1de158
Vladimir Jotov <vjotov@saritasa.com>
2017-12-18 16:05:26 +0700

    initial commit

---
+++ src/main/java/com/jotov/skyrunrating/SkyrunratingApplication.java
@@ -0,0 +1,12 @@
+package com.jotov.skyrunrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyrunratingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyrunratingApplication.class, args);
+	}
+}
