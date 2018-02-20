bba95f4a0bf5b914722a35bf3513f2d11d227e0d
Vladimir Jotov <v.jotov@gmail.com>
2018-02-20 17:04:53 +0700

    try to write unit test

--- src/test/java/com/jotov/skyrunrating/CompetitionServiceTest.java
+++ src/test/java/com/jotov/skyrunrating/CompetitionServiceTest.java
@@ -1,23 +1,22 @@
 package com.jotov.skyrunrating;
 
-import com.jotov.skyrunrating.service.CompetitionService;
-import com.jotov.skyrunrating.repository.CompetiotionRepository;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.mockito.Mockito.*;
+//import com.jotov.skyrunrating.repository.CompetiotionRepository;
+//import com.jotov.skyrunrating.service.CompetitionService;
+//import org.junit.Before;
+//
+//import static org.mockito.Mockito.mock;
 
 public class CompetitionServiceTest {
-    private CompetitionService competitionService;
-    private CompetiotionRepository competitionRepositoryMock;
-
-    @Before
-    public void setUp(){
-        competitionService = new  CompetitionService();
-
-        competitionRepositoryMock = mock(CompetiotionRepository.class);
-        competitionService.setCompetiotionRepository(competitionRepositoryMock);
-    }
+//    private CompetitionService competitionService;
+//    private CompetiotionRepository competitionRepositoryMock;
+//
+//    @Before
+//    public void setUp(){
+//        competitionService = new  CompetitionService();
+//
+//        competitionRepositoryMock = mock(CompetiotionRepository.class);
+//       // competitionService.setCompetiotionRepository(competitionRepositoryMock);
+//    }
 
     // to read: https://www.petrikainulainen.net/programming/spring-framework/spring-data-jpa-tutorial-three-custom-queries-with-query-methods/
 //    @Test
